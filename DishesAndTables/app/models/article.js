48ffee5f7226911b8de082281815b8c57cb53cc1
Elena-Ovechkina <lena15brain06u>
2021-06-04 22:32:12 +0300

    Создали модель Dishes

--- DishesAndTables/app/models/article.js
+++ DishesAndTables/app/models/article.js
@@ -3,14 +3,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ArticleSchema = new Schema({
-  title: String,
-  url: String,
-  text: String
+const Dishes = new Schema({
+  listOfIngridients: [String],
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  typeOfDishes: String,
+  photo: String,
+  description: String,
+  availability: Boolean,
+  tags: [String],
+  cost: {
+    type: Number,
+    min: 0
+  }
 });
 
-ArticleSchema.virtual('date')
-  .get(() => this._id.getTimestamp());
-
-mongoose.model('Article', ArticleSchema);
-
+mongoose.model('Dishes', Dishes);
