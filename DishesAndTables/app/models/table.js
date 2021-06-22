809c80c07ac08b1374c72c6958a4c474def7224d
Elena-Ovechkina <70863711+Elena-Ovechkina@users.noreply.github.com>
2021-06-22 21:21:37 +0300

    Создали модель Table

--- DishesAndTables/app/models/table.js
+++ DishesAndTables/app/models/table.js
@@ -1,1 +1,26 @@
-//  Описать типовой документ представляющий информацию о столиках и их бронированнии
+//  Типовой документ, представляющий информацию о столиках и их
+//  бронированнии
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Table = new Schema({
+    name: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    timeOfBooking: [Number],
+    quantity: Number,
+    availability: {
+        type: Boolean,
+        default: true
+    },
+    tags: [String],
+    cost: {
+        type: Number,
+        min: 0
+    }
+});
+
+const model = mongoose.model('Table', Table);
